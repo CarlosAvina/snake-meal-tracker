e549c711a5110acd93f7e21083ad7a6d265d7a01
Carlos Aviña <juan.avina@wizeline.com>
2023-06-17 22:10:32 -0500

    Add cron job

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,6 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log("Cron job");
+    res.status(200).end('Cool!');
+}
