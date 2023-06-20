2e8b9af7064644e8953df0ab7c6a5776ce514a0a
Carlos Aviña <juan.avina@wizeline.com>
2023-06-19 22:44:01 -0500

    Get which snakes need to feed

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,6 +1,29 @@
+import moment from "moment";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "~/server/db";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log("Cron job");
-    res.status(200).end('Cool!');
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const data = await prisma.snake.findMany({
+        select: {
+            name: true, lastMeal: true
+        }
+    });
+
+    const snakesToFeed: string[] = [];
+
+    data.forEach((snake) => {
+        const nextMeal = moment(snake.lastMeal).add(10, 'days');
+        const daysToNextMeal = nextMeal.diff(moment(), 'days');
+
+        if (daysToNextMeal < 3) {
+            snakesToFeed.push(snake.name);
+        }
+    });
+
+    if (snakesToFeed.length > 0) {
+        console.log('Sending email to ', snakesToFeed.join(','));
+        res.status(200).end('Send email!');
+    }
+
+    console.log('Not sending email');
 }
