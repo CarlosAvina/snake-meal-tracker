1fe124a44f30c93b7a635281b013f8ea684aeed0
Carlos Aviña <juan.avina@wizeline.com>
2023-08-12 21:13:06 -0500

    Update email template

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -1,11 +1,21 @@
 interface EmailTemplateProps {
-    snakeName: string;
+  snakes: Array<{ name: string, daysToNextMeal: number }>;
 }
 
 export const EmailTemplate = ({
-    snakeName,
+  snakes,
 }: EmailTemplateProps) => (
-    <div>
-        <h1>It&apos;s time to feed {snakeName}!</h1>
+  <div>
+    <h1>It&apos;s time to feed your snakes!</h1>
+    <div className="grid grid-cols-2 justify-center items-center">
+      <h2 className="font-bold text-2xl">Snake</h2>
+      <h2 className="font-bold text-2xl">Next meal (days)</h2>
+      {snakes.map((snake) => {
+        return <>
+          <p>{snake.name}</p>
+          <p>{snake.daysToNextMeal}</p>
+        </>
+      })}
     </div>
+  </div>
 );
--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -11,47 +11,44 @@ const receiverEmail = process.env.RESEND_RECEIVER_EMAIL || "";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    try {
-        const data = await prisma.snake.findMany({
-            select: {
-                name: true, lastMeal: true
-            }
-        });
+  try {
+    const data = await prisma.snake.findMany({
+      select: {
+        name: true, lastMeal: true
+      }
+    });
 
-        const snakesToFeed: string[] = [];
+    const snakesToFeed: Array<{ name: string, daysToNextMeal: number }> = [];
 
-        data.forEach((snake) => {
-            const nextMeal = moment(snake.lastMeal).add(10, 'days');
-            const daysToNextMeal = nextMeal.diff(moment(), 'days');
+    data.forEach((snake) => {
+      const nextMeal = moment(snake.lastMeal).add(10, 'days');
+      const daysToNextMeal = nextMeal.diff(moment(), 'days');
 
-            if (daysToNextMeal < 3) {
-                snakesToFeed.push(snake.name);
-            }
-        });
+      if (daysToNextMeal < 3) {
+        snakesToFeed.push({ name: snake.name, daysToNextMeal });
+      }
+    });
 
-        if (snakesToFeed.length > 0) {
-            const snakeNames = snakesToFeed.join(', ');
+    if (snakesToFeed.length > 0) {
+      const data = await resend.emails.send({
+        from: senderEmail,
+        to: receiverEmail,
+        subject: "Time to feed your snakes",
+        react: EmailTemplate({ snakes: snakesToFeed }),
+      });
 
-            const data = await resend.emails.send({
-                from: senderEmail,
-                to: receiverEmail,
-                subject: "Time to feed your snakes",
-                html: "<strong>It works!</strong>",
-                react: EmailTemplate({ snakeName: snakeNames }),
-            });
-
-            res.status(200).json(data);
-        }
-
-        await resend.emails.send({
-            from: senderEmail,
-            to: receiverEmail,
-            subject: "Time to feed your snakes",
-            html: "<strong>It works!</strong>",
-        });
-
-        res.status(200).json({ message: 'Not sending email' });
-    } catch (error) {
-        res.status(400).json(error);
+      res.status(200).json(data);
     }
+
+    await resend.emails.send({
+      from: senderEmail,
+      to: receiverEmail,
+      subject: "You can relax",
+      html: "<strong>Your snakes are ok :D</strong>",
+    });
+
+    res.status(200).json({ message: 'Not sending email' });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 }
