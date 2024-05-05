c588ae40e893c7b1b5e5f33b397169854c5e3bb1
Carlos Aviña <carlosavilu11@gmail.com>
2024-05-04 20:16:10 -0600

    Update path of cron job

--- app/actions/updateLastMeal.ts
+++ app/actions/updateLastMeal.ts
@@ -2,6 +2,5 @@
 import { sql } from "@vercel/postgres";
 
 export default async function updateLastMeal(snakeId: number, newDate: string) {
-  console.log(newDate);
   await sql`UPDATE snakes SET lastmeal = ${newDate} WHERE snakes.id = ${snakeId}`;
 }
