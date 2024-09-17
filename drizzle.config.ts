ee878bbf5cb5f8c493ef397d3426ff08575096e8
Carlos Aviña <carlosavilu11@gmail.com>
2024-09-16 19:20:00 -0600

    Ignore type errors in drizzle file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./drizzle/schema.ts",
+  // @ts-ignore
   driver: "pg",
   dbCredentials: {
+    // @ts-ignore
     connectionString: process.env.POSTGRES_URL!,
   },
 });
