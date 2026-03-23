e259c819b3916cddc21a02ef734d286b087ee49c
Carlos Avina <carlosavilu11@gmail.com>
2026-03-22 18:24:31 -0600

    add ecosystem file

---
+++ api/ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "pins",
+      script: "./index.js",
+      cwd: "/root/code/snake-meal-tracker/api",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "256M",
+      env_file: ".env",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
